a8e893cd71449a7d19be3058579099286e4abef4
rfrm99 <rfrm99@users.noreply.github.com>
2020-03-06 19:57:38 +0700

    add product of all elements in array

--- Semester 2/Mr. Jude/ArrayExercises/ArrayExercise.java
+++ Semester 2/Mr. Jude/ArrayExercises/ArrayExercise.java
@@ -92,14 +92,17 @@ class ArrayExercise {
         System.out.println("No. 4");
         int[] x3 = new int[5];
         int sum = 0;
+        int prod = 1;
         for(int i = 0; i < x3.length; i++) {
             System.out.println("Enter number " + (i + 1) + ": ");
             x3[i] = scan.nextInt();
         }
         for(int i = 0; i < x3.length; i++) {
             sum += x3[i];
+            prod = prod*x3[i];
         }
         System.out.println("Sum of all elements: "+sum);
+        System.out.println("Product of all elements: " +prod);
 
         //5
         System.out.println("No. 5");
