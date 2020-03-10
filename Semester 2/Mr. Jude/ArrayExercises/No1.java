f902cccc7152331d671d0eb433c01d41fbddc833
rfrm99 <rfrm99@users.noreply.github.com>
2020-03-10 16:06:58 +0700

    small edits on strings

--- Semester 2/Mr. Jude/ArrayExercises/No1.java
+++ Semester 2/Mr. Jude/ArrayExercises/No1.java
@@ -6,7 +6,7 @@ class No1 {
         int[] x1 = new int[10]; //creates an array
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("No. 1 and 2");
+        System.out.println("No. 1");
         for (int i = 0; i < x1.length; i++) {
             System.out.println("Enter number " + (i + 1) + ": ");
             x1[i] = scan.nextInt();
--- Semester 2/Mr. Jude/ArrayExercises/No2.java
+++ Semester 2/Mr. Jude/ArrayExercises/No2.java
@@ -24,7 +24,7 @@ class No2 {
         int[] x1 = new int[10]; //creates an array
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("No. 1 and 2");
+        System.out.println("No. 2");
         for(int i = 0; i < x1.length; i++) {
             System.out.println("Enter number " + (i+1) + ": " );
             x1[i] = scan.nextInt();
